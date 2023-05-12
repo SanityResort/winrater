3b18483aa1bdf77c56f4c030acb7edebae4e89be
Christian Huber <huberc@butterbrot.org>
2023-05-12 23:56:20 +0200

    Adjust test to expect combined response

--- src/coachLocator/__tests__/service.spec.ts
+++ src/coachLocator/__tests__/service.spec.ts
@@ -1,11 +1,11 @@
-import type {Mock} from 'vitest'
+import type { Mock } from "vitest";
 
 import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 import { load } from "../service";
 
 describe("coach lookup service", () => {
 
-  let mock: Mock<any, any>
+  let mock: Mock<any, any>;
 
   beforeAll(() => {
     mock = vi.fn().mockImplementation(window.fetch);
@@ -19,17 +19,32 @@ describe("coach lookup service", () => {
   it("calls fumbbl api", async () => {
 
     mock.mockImplementationOnce((url: string) => {
-      return new Response(JSON.stringify(initialResponse));
-    })
+      let response: { id: number }[] = [];
+      if (url.endsWith("/name")) {
+        response = initialResponse;
+      } else if (url.endsWith("/name/2")) {
+        response = intermediateResponse;
+      } else if (url.endsWith("/name/0")) {
+        response = finalResponse;
+      }
 
-    const response: [] = await load("name");
+      return new Response(JSON.stringify(response));
+    });
 
-    expect(window.fetch).toBeCalledWith("https://fumbbl.com/api/match/list/name")
+    const response: { id: number }[] = await load("name");
 
-    expect(response).toMatchObject(initialResponse)
+    expect(window.fetch).toBeCalledTimes(3);
+
+    expect(window.fetch).toBeCalledWith("https://fumbbl.com/api/match/list/name");
+    expect(window.fetch).toBeCalledWith("https://fumbbl.com/api/match/list/name/2");
+    expect(window.fetch).toBeCalledWith("https://fumbbl.com/api/match/list/name/0");
+
+    const expected: { id: number }[] = [ {id: 3 }, { id: 2 }, { id: 1 }, { id: 0 }];
+
+    expect(response).toEqual(expected);
   });
 });
 
-const initialResponse = [{ id: 3 }, {id: 2}]
-const intermediateResponse = [{ id: 2 }, {id: 1}, {id: 0}]
-const finalResponse = [{ id: 0 }]
+const initialResponse = [{ id: 3 }, { id: 2 }];
+const intermediateResponse = [{ id: 2 }, { id: 1 }, { id: 0 }];
+const finalResponse = [{ id: 0 }];
