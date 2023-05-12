208597b7e289a73ff12bea2c454cfc71f4a0f2c5
Christian Huber <huberc@butterbrot.org>
2023-05-12 23:31:15 +0200

    Adjust test to verify an actual response

--- src/coachLocator/__tests__/service.spec.ts
+++ src/coachLocator/__tests__/service.spec.ts
@@ -1,20 +1,35 @@
+import type {Mock} from 'vitest'
+
 import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 import { load } from "../service";
 
 describe("coach lookup service", () => {
 
+  let mock: Mock<any, any>
+
   beforeAll(() => {
-    window.fetch = vi.fn().mockImplementation(window.fetch)
+    mock = vi.fn().mockImplementation(window.fetch);
+    window.fetch = mock;
   });
 
   afterEach(() => {
     vi.restoreAllMocks();
   });
 
-  it("calls fumbbl api", () => {
+  it("calls fumbbl api", async () => {
 
-    load("name");
+    mock.mockImplementationOnce((url: string) => {
+      return new Response(JSON.stringify(initialResponse));
+    })
+
+    const response: [] = await load("name");
 
     expect(window.fetch).toBeCalledWith("https://fumbbl.com/api/match/list/name")
+
+    expect(response).toMatchObject(initialResponse)
   });
 });
+
+const initialResponse = [{ id: 3 }, {id: 2}]
+const intermediateResponse = [{ id: 2 }, {id: 1}, {id: 0}]
+const finalResponse = [{ id: 0 }]
--- src/coachLocator/service.ts
+++ src/coachLocator/service.ts
@@ -1,7 +1,5 @@
-import type { Ref, UnwrapRef } from "vue";
-
 const BASE_URL = "https://fumbbl.com/api/match/list/"
 
-export async function load(coachName: string): Promise<any> {
+export async function load(coachName: string): Promise<[]> {
   return (await window.fetch(BASE_URL + coachName)).json()
 }
