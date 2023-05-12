f55216a0a0606bb0e87a1aed0c12124dfb012944
Christian Huber <huberc@butterbrot.org>
2023-05-12 22:42:49 +0200

    Remove scaffold components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import CoachLookup from "./coachLocator/Component.vue";
+import { ref } from 'vue'
+const matchData = ref([])
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+
   </header>
 
   <main>
-    <TheWelcome />
+    <CoachLookup @matches='((matches) => { matchData = matches})'/>
+
+    {{matchData}}
   </main>
 </template>
 
@@ -22,10 +23,7 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+
 
 @media (min-width: 1024px) {
   header {
@@ -34,9 +32,6 @@ header {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
   header .wrapper {
     display: flex;
