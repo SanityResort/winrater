e37a0dd8b6af38c0422e076d8130d7101f226efe
Christian Huber <huberc@butterbrot.org>
2023-05-14 22:21:29 +0200

    Implement score mapping

--- src/rating/mapper.ts
+++ src/rating/mapper.ts
@@ -1,45 +1,53 @@
 import { Category, Score } from "./match";
 
 export function category(input: FumbblMatch): Category {
-    switch (input.division) {
-        case "Competitive" :
-            if (input.scheduler === "Blackbox") {
-                return Category.Blackbox;
-            }
-            return Category.Competitive;
-        case "Blackbox":
-            return Category.Legacy_Blackbox;
-        case "League":
-            return Category.League;
-        case "Ranked":
-            return Category.Ranked;
-        case "Unranked":
-            return Category.Unranked;
-        case "Stunty Leeg":
-            return Category.Stunty_Leeg;
-        case "DivX Legacy":
-            return Category.DivX;
-        case "Academy":
-            return Category.Academy;
-        case "Faction":
-            return Category.Faction;
-        case "FFB Test":
-            return Category.FFB_Test;
-        case "Fantasy Football":
-            return Category.Fantasy_Football;
-        case "LRB4":
-            return Category.LRB4;
-        case "Transfer Division":
-            return Category.Transfer;
-        case "Transfer Division 2":
-            return Category.Transfer2;
-        case "Ladder":
-            return Category.Ladder;
-        default:
-            return Category.Unknown
-    }
+  switch (input.division) {
+    case "Competitive" :
+      if (input.scheduler === "Blackbox") {
+        return Category.Blackbox;
+      }
+      return Category.Competitive;
+    case "Blackbox":
+      return Category.Legacy_Blackbox;
+    case "League":
+      return Category.League;
+    case "Ranked":
+      return Category.Ranked;
+    case "Unranked":
+      return Category.Unranked;
+    case "Stunty Leeg":
+      return Category.Stunty_Leeg;
+    case "DivX Legacy":
+      return Category.DivX;
+    case "Academy":
+      return Category.Academy;
+    case "Faction":
+      return Category.Faction;
+    case "FFB Test":
+      return Category.FFB_Test;
+    case "Fantasy Football":
+      return Category.Fantasy_Football;
+    case "LRB4":
+      return Category.LRB4;
+    case "Transfer Division":
+      return Category.Transfer;
+    case "Transfer Division 2":
+      return Category.Transfer2;
+    case "Ladder":
+      return Category.Ladder;
+    default:
+      return Category.Unknown;
+  }
 }
 
 export function score(input: FumbblMatch, coachName: string): Score {
+  if (input.team1.score === input.team2.score) {
+    return Score.Draw;
+  }
 
+  if ((input.team1.score > input.team2.score) === (input.team1.coach.name === coachName)) {
+    return Score.Win;
+  }
+
+  return Score.Loss;
 }
