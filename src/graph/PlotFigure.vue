9a694f174d0f150838b48dfba5a3ebc3ede58321
Christian Huber <huberc@butterbrot.org>
2023-06-04 17:03:16 +0200

    Add dot indicator

--- src/graph/PlotFigure.vue
+++ src/graph/PlotFigure.vue
@@ -17,11 +17,48 @@ const render = () => {
 
           const callbacks: Map<string, (event: MouseEvent) => void> = props.callbacks
           const plotDom = d3.select(plot)
-          console.log('# of callbacks:' + callbacks.size)
           for (let callback of callbacks) {
-            console.log('Callback ' + callback)
             plotDom.on(callback[0], callback[1])
           }
+          const dot = plotDom.append('g').attr('display', 'none')
+          dot.append('circle').attr('r', 2)
+
+          plotDom.on('pointerenter', () => {
+            dot.attr('display', null)
+          })
+
+          plotDom.on('pointerleave', () => {
+            dot.attr('display', 'none')
+          })
+
+          plotDom.on('pointermove', (event: MouseEvent) => {
+            const xPx = plot.scale('x')?.apply
+            const yPx = plot.scale('y')?.apply
+            const [ex, ey] = d3.pointer(event)
+            const closest = d3.least(
+              props.options.marks
+                .filter((mark) => mark.z)
+                .flatMap((mark) => {
+                  return mark.data.map((data) => {
+                    const x = xPx ? xPx(data.index) : 0
+                    const y = yPx ? yPx(data.ratio * 100) : 0
+                    return {
+                      x: x,
+                      y: y,
+                      z: mark.z,
+                      stroke: mark.stroke,
+                      distance: Math.hypot(x - ex, y - ey)
+                    }
+                  })
+                }),
+              (point) => point.distance
+            )
+            dot
+              .attr('transform', `translate(${closest.x},${closest.y})`)
+              .attr('stroke', closest.stroke)
+              .attr('fill', closest.stroke)
+              .attr('display', null)
+          })
         }
       }
     ]
