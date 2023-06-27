cd1062cda15b77dea4327614c3655fdf7ed69fdd
Christian Huber <huberc@butterbrot.org>
2023-06-27 23:15:57 +0200

    Filter categories from matches

--- src/rating/Component.vue
+++ src/rating/Component.vue
@@ -2,6 +2,8 @@
 import { Store } from '@/rating/store'
 import { useMatchStore } from '@/pinia/store'
 import { storeToRefs } from 'pinia'
+import { computed, ComputedRef } from 'vue'
+import type { Category } from '@/rating/match'
 
 const props = defineProps({
   coachName: String
@@ -13,10 +15,22 @@ const { stores } = storeToRefs(matchStore)
 const key: string = props.coachName?.toString()
 const store: Store = stores.value.get(key)
 const matches = store.matches
+const categories: ComputedRef<Category[]> = computed(() => {
+  const result: Category[] = []
+  matches
+    .map((match) => match.category)
+    .forEach((category) => {
+      if (result.indexOf(category) < 0) {
+        result.push(category)
+      }
+    })
+  return result.sort((a: Category, b: Category) => a.toString().localeCompare(b.toString()))
+})
 </script>
 
 <style scoped></style>
 
 <template>
   <div class="store">{{ coachName }}: {{ matches.length }}</div>
+  <div>{{ categories }}</div>
 </template>
