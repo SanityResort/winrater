fe60a8ffb798f8f2ccb3fb1bc8176e7173a160b3
Christian Huber <huberc@butterbrot.org>
2023-08-27 02:03:24 +0200

    Define label colors

--- src/rating/match.ts
+++ src/rating/match.ts
@@ -17,73 +17,73 @@ export const Blackbox = {
   name: 'Blackbox',
   valid: true,
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  background: Color.rgb({ r: 0, g: 0, b: 0 })
 } as const satisfies Category
 export const Competitive = {
   name: 'Competitive',
   valid: true,
-  foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 0, g: 255, b: 0 })
+  foreground: Color.rgb({ r: 0, g: 0, b: 0 }),
+  background: Color.rgb({ r: 255, g: 250, b: 106 })
 } as const satisfies Category
 export const League = {
   name: 'League',
   valid: true,
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 0, g: 0, b: 255 })
+  background: Color.rgb({ r: 179, g: 8, b: 71 })
 } as const satisfies Category
 export const Ranked = {
   name: 'Ranked',
   valid: true,
-  foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  foreground: Color.rgb({ r: 0, g: 0, b: 0 }),
+  background: Color.rgb({ r: 180, g: 255, b: 0 })
 } as const satisfies Category
 export const Unranked = {
   name: 'Unranked',
   valid: true,
-  foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  foreground: Color.rgb({ r: 0, g: 0, b: 0 }),
+  background: Color.rgb({ r: 255, g: 212, b: 0 })
 } as const satisfies Category
 export const Legacy_Blackbox = {
   name: 'Blackbox 2016',
   valid: true,
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  background: Color.rgb({ r: 43, g: 29, b: 122 })
 } as const satisfies Category
 export const Academy = {
   name: 'Academy',
   valid: true,
-  foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  foreground: Color.rgb({ r: 0, g: 0, b: 0 }),
+  background: Color.rgb({ r: 99, g: 233, b: 255 })
 } as const satisfies Category
 export const Faction = {
   name: 'Faction',
   valid: true,
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  background: Color.rgb({ r: 26, g: 94, b: 31 })
 } as const satisfies Category
 export const Stunty_Leeg = {
   name: 'Stunty Leeg',
   valid: true,
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  background: Color.rgb({ r: 241, g: 118, b: 31 })
 } as const satisfies Category
 export const DivX = {
   name: 'DivX',
   valid: true,
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  background: Color.rgb({ r: 255, g: 0, b: 247 })
 } as const satisfies Category
 export const FFB_Test = {
   name: 'FFB Test',
   valid: false,
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  background: Color.rgb({ r: 149, g: 142, b: 142 })
 } as const satisfies Category
 export const Fantasy_Football = {
   name: 'Legacy Test',
   valid: false,
-  foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  foreground: Color.rgb({ r: 86, g: 86, b: 86 }),
+  background: Color.rgb({ r: 215, g: 215, b: 215 })
 } as const satisfies Category
 export const LRB4 = {
   name: 'LRB4',
@@ -107,7 +107,7 @@ export const Ladder = {
   name: 'Ladder',
   valid: true,
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
-  background: Color.rgb({ r: 255, g: 0, b: 0 })
+  background: Color.rgb({ r: 133, g: 21, b: 255 })
 } as const satisfies Category
 export const Unknown = {
   name: 'Unknown',
@@ -115,23 +115,3 @@ export const Unknown = {
   foreground: Color.rgb({ r: 255, g: 255, b: 255 }),
   background: Color.rgb({ r: 255, g: 0, b: 0 })
 } as const satisfies Category
-
-export const allCategories: Category[] = [
-  Blackbox,
-  Competitive,
-  League,
-  Legacy_Blackbox,
-  Faction,
-  Fantasy_Football,
-  Ranked,
-  Unranked,
-  Academy,
-  Unranked,
-  Ladder,
-  Transfer,
-  Transfer2,
-  LRB4,
-  Stunty_Leeg,
-  FFB_Test,
-  DivX
-]
